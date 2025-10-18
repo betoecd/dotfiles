05b44286ba3a413af4e112b125f775a876c67078
Eberton Dias <eberton.bits@gmail.com>
2025-10-18 06:58:34 -0300

    chore: test rose-pine colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -13,3 +13,22 @@ return {
         end,
     },
 }
+
+-- return {
+--     "rose-pine/neovim",
+--     name = "rose-pine-moon",
+--     config = function()
+--         require("rose-pine").setup({
+--             variant = "moon",
+--             disable_background = true,
+--             disable_float_background = true,
+--             disable_italics = true,
+--             dim_inactive = {
+--                 enabled = true,
+--                 shade = "dark",
+--                 percentage = 0.15,
+--             },
+--         })
+--         vim.cmd.colorscheme("rose-pine-moon")
+--     end,
+-- }
