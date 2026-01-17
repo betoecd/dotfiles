7006251e8fed1c50520d476b94ea89d9e990b1dd
Eberton Dias <eberton.bits@gmail.com>
2026-01-17 07:03:19 -0300

    chore: update colorscheme settings

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,29 @@
 return {
+    -- {
+    --     "adibhanna/yukinord.nvim",
+    --     priority = 1000,
+    --     config = function()
+    --         vim.cmd.colorscheme("yukinord")
+    --     end,
+    -- },
+    -- {
+    --     "rose-pine/neovim",
+    --     name = "rose-pine-moon",
+    --     config = function()
+    --         require("rose-pine").setup({
+    --             variant = "moon",
+    --             disable_background = true,
+    --             disable_float_background = true,
+    --             disable_italics = true,
+    --             dim_inactive = {
+    --                 enabled = true,
+    --                 shade = "dark",
+    --                 percentage = 0.15,
+    --             },
+    --         })
+    --         vim.cmd.colorscheme("rose-pine-moon")
+    --     end,
+    -- },
     {
         "catppuccin/nvim",
         lazy = false,
@@ -7,28 +32,16 @@ return {
 
         config = function()
             require("catppuccin").setup({
+                flavour = "mocha",
                 transparent_background = true,
+                no_italic = true,
+                dim_inactive = {
+                    enabled = true,
+                    shade = "dark",
+                    percentage = 0.15,
+                },
             })
             vim.cmd.colorscheme("catppuccin")
         end,
     },
 }
-
--- return {
---     "rose-pine/neovim",
---     name = "rose-pine-moon",
---     config = function()
---         require("rose-pine").setup({
---             variant = "moon",
---             disable_background = true,
---             disable_float_background = true,
---             disable_italics = true,
---             dim_inactive = {
---                 enabled = true,
---                 shade = "dark",
---                 percentage = 0.15,
---             },
---         })
---         vim.cmd.colorscheme("rose-pine-moon")
---     end,
--- }
