676b9a1eb6da5459c02c0964cd52f19e7a22062a
Eberton Dias <eberton.bits@gmail.com>
2026-01-20 18:22:48 -0300

    chore: add more colorscheme options to nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,68 @@
 return {
-    -- {
-    --     "adibhanna/yukinord.nvim",
-    --     priority = 1000,
-    --     config = function()
-    --         vim.cmd.colorscheme("yukinord")
-    --     end,
-    -- },
-    -- {
-    --     "rose-pine/neovim",
-    --     name = "rose-pine-moon",
-    --     config = function()
-    --         require("rose-pine").setup({
-    --             variant = "moon",
-    --             disable_background = true,
-    --             disable_float_background = true,
-    --             disable_italics = true,
-    --             dim_inactive = {
-    --                 enabled = true,
-    --                 shade = "dark",
-    --                 percentage = 0.15,
-    --             },
-    --         })
-    --         vim.cmd.colorscheme("rose-pine-moon")
-    --     end,
-    -- },
+    {
+        "adibhanna/yukinord.nvim",
+        priority = 1000,
+        config = function()
+            -- vim.cmd.colorscheme("yukinord")
+        end,
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine-moon",
+        config = function()
+            require("rose-pine").setup({
+                variant = "moon",
+                disable_background = true,
+                disable_float_background = true,
+                disable_italics = true,
+                dim_inactive = {
+                    enabled = true,
+                    shade = "dark",
+                    percentage = 0.15,
+                },
+            })
+            vim.cmd.colorscheme("rose-pine-moon")
+        end,
+    },
+    {
+        "nyoom-engineering/oxocarbon.nvim",
+        -- Add in any other configuration;
+        --   event = foo,
+        --   config = bar
+        --   end,
+    },
+    {
+        "sainnhe/everforest",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- Optionally configure and load the colorscheme
+            -- directly inside the plugin declaration.
+            vim.g.everforest_enable_italic = true
+            -- vim.cmd.colorscheme('everforest')
+        end,
+    },
+    {
+        "craftzdog/solarized-osaka.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = function()
+            return {
+                transparent = true,
+            }
+        end,
+    },
+    {
+        "datsfilipe/vesper.nvim",
+        lazy = false,
+        priority = 1000,
+
+        opts = function()
+            return {
+                transparent = true,
+            }
+        end,
+    },
     {
         "catppuccin/nvim",
         lazy = false,
@@ -41,7 +80,7 @@ return {
                     percentage = 0.15,
                 },
             })
-            vim.cmd.colorscheme("catppuccin")
+            -- vim.cmd.colorscheme("catppuccin")
         end,
     },
 }
