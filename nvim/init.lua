eb8a49a5556dd7851407e72833ead6c908b5d39c
Eberton Dias <eberton.bits@gmail.com>
2026-01-22 06:21:45 -0300

    fix: update nvim file change tmux panes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.opt.autoread = true
+
+vim.api.nvim_create_augroup("AutoReload", { clear = true })
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+    command = "checktime",
+    group = "AutoReload",
+})
